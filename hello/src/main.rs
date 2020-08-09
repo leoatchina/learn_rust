ab7140cb4130a4f367401fc0c6925edf8314d1b5
leoatchina <leoatchina@outlook.com>
2020-08-09 11:05:29 +0800

    update

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,14 @@
+/**
+ * File              : main.rs
+ * Author            : leoatchina <leoatchina@outlook.com>
+ * Date              : 2020.08.09
+ * Last Modified Date: 2020.08.09
+ * Last Modified By  : leoatchina <leoatchina@outlook.com>
+ */
+
 fn main() {
+    let a = 10;
+    let b = 5;
     println!("Hello, world!");
+    println!("{}", a + b);
 }
